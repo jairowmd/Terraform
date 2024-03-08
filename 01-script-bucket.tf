1597423e647c825f995a346f75c7b978837ad449
Jairo <105881237+jairowmd@users.noreply.github.com>
2024-03-07 21:28:34 -0300

    Update 01-script-bucket.tf

--- 01-script-bucket.tf
+++ 01-script-bucket.tf
@@ -1,5 +1,24 @@
 # criação de bucket simples via terraform ja com o AWS cli configurado com o provider
 
+
+# versao do terraform e do aplicativo aws para terraform 
+terraform {
+  required_version = "1.7.4"
+
+required_providers {
+aws = {
+    source = "hashicorp/aws"
+    version = "5.39.1"
+}
+  
+}
+
+}
+
+
+
+#  Using previously-installed hashicorp/aws v5.39.1
+
 provider "aws" {
   region = "us-east-1"
   # por padrao pega o profile padrao
@@ -17,6 +36,4 @@ resource "aws_s3_bucket" "bucket-terraform-teste" {
   }
 }
 
-# C:\terraform>
-#  terraform init
-#  terraform plan
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
